68278f905bc6b56780deb14c53eeccc5e83157c6
Pedram Kayedpour <pkayedp1@jh.edu>
2026-07-22 16:47:31 -0400

    Change device.py to use QtSerial instead of pyserial
    Allows for better integration with the Qt ecosystem. it is also
    non-blocking.
    
    Maybe a bit of an unecessary change especially since I could write a
    wrapper around pyserial to handle the blocking part of the code into a
    one way I/O.

--- src/hardware/device.py
+++ src/hardware/device.py
@@ -1,10 +1,12 @@
-import serial
+from PySide6.QtCore import QIODeviceBase
+from PySide6.QtSerialPort import QSerialPort
 import serial.tools.list_ports
 
 # grblHAL BOB controller VID and PID. 
-TARGET_VID = int('0483', 16)
-TARGET_PID = int('5740', 16)
+TARGET_VID    = int('0483', 16)
+TARGET_PID    = int('5740', 16)
 TARGET_SERIAL = '4E814D985200'
+TARGET_BAUD   = 115200
 
 # Fine the device port currently used by the BOB.
 def find_device_port():
@@ -26,22 +28,27 @@ def connect():
         print("Device not found. Check connection.")
         return None
 
-    try:
-        ser = serial.Serial(port=target, baudrate=115200, timeout=2, rtscts=False)
+    port = QSerialPort()
+    port.setPortName(target)
+    port.setBaudRate(TARGET_BAUD)
+    port.setDataBits(QSerialPort.DataBits.Data8)
+    port.setParity(QSerialPort.Parity.NoParity)
+    port.setStopBits(QSerialPort.StopBits.OneStop)
+    port.setFlowControl(QSerialPort.FlowControl.NoFlowControl)
 
-    except serial.SerialException as e:
-        print(f"Failed to open {target}: {e}")
+    if not port.open(QIODeviceBase.OpenModeFlag.ReadWrite):
+        print(f"Failed to open {target}: {port.errorString()}")
         return None
 
-    return ser
+    return port
 
 # Unit test for insuring the code within this file is running correctly.
 if __name__ == "__main__":
     ser = connect()
     try:
-        print("Port open:", ser.is_open if ser else False)
+        print("Port open:", ser.isOpen() if ser else False)
         if ser:
-            print("Device:", ser.name)
+            print("Device:", ser.portName())
     finally:
         if ser:
             ser.close()
