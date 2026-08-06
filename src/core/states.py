80330e5f4c5246087ced4d39dff189943f98b94a
Pedram Kayedpour <pkayedp1@jh.edu>
2026-08-06 16:37:52 -0400

    Add buttons and parameters to control the chair.
    Refactor `connection.py` to expose `ConnectionService` as both the
    thread and the worker combined.
    
    Create `ui_controller.py` to seperate ui behavior and looks.
    
    We want to be able to iterate on this software later. The important
    thing here is not how fast we can get this up and running, but how well
    can we maintain this in the long run. Getting a good foundation for a
    work like this thus becomes extremely important.

---
+++ src/core/states.py
@@ -0,0 +1,13 @@
+from enum import Enum, auto
+
+class ConnectionState(Enum):
+    DISCONNECTED = auto()
+    CONNECTING = auto()
+    CONNECTED = auto()
+    FAILED = auto()
+    STOPPING = auto()
+
+
+class UICState(Enum):
+    ALLOW_INPUT = auto()
+    DISALLOW_INPUT = auto()
--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,58 +1,121 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QWidget
+import argparse
+import logging
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QGridLayout, QDoubleSpinBox, QVBoxLayout, QHBoxLayout, QWidget, QStyle
+from core.states import ConnectionState as CState
+from core.protocol import MAX_DIST
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", action="count", default=0)
+args = parser.parse_args()
 
-from core.workers import start_worker, DeviceWorker
+levels = [logging.WARNING, logging.INFO, logging.DEBUG]
+level = levels[min(args.verbose, len(levels) - 1)]
+
+logging.basicConfig(level=level, format="%(asctime)s %(levelname)s %(name)s: %(message)s")
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    
+    def __init__(self, controller):
         super().__init__()
-        self.device_connection = None
+        self.controller = controller
 
-        self.connection_status = QLabel("Device not connected.")
+        self._connection_status = QLabel('Device not connected.')
+        controller.state_changed.connect(self._state_changed)
 
-        self.connection_button = QPushButton("Connect")
-        self.connection_button.clicked.connect(self.connection_button_callback)
+        self._connection_button = QPushButton('Connect')
+        self._connection_button.clicked.connect(self.controller.connection_button_callback)
+
+        self._magnitude_box = QDoubleSpinBox()
+        self._magnitude_box.setDecimals(1)
+        self._magnitude_box.setRange(0.0, MAX_DIST)
+
+        self._z_plus_button = QPushButton('Up')
+        self._z_minus_button = QPushButton('Down')
+        self._a_plus_button = QPushButton('Right')
+        self._a_minus_button = QPushButton('Left')
+
+        self._z_plus_button.clicked.connect(lambda: self.controller.send_jog_line('Z+', self._magnitude_box.value()))
+        self._z_minus_button.clicked.connect(lambda: self.controller.send_jog_line('Z-', self._magnitude_box.value()))
+        self._a_plus_button.clicked.connect(lambda: self.controller.send_jog_line('A+', self._magnitude_box.value()))
+        self._a_minus_button.clicked.connect(lambda: self.controller.send_jog_line('A-', self._magnitude_box.value()))
+
+        self._jog_cancel_button = QPushButton('Cancel')
+        self._jog_cancel_button.clicked.connect(self.controller.send_jog_cancel)
+
+        # self._z_plus_button.pressed.connect(lambda: self.controller.arrow_button_pressed('Z+'))
+        # self._z_minus_button.pressed.connect(lambda: self.controller.arrow_button_pressed('Z-'))
+        # self._a_plus_button.pressed.connect(lambda: self.controller.arrow_button_pressed('A+'))
+        # self._a_minus_button.pressed.connect(lambda: self.controller.arrow_button_pressed('A-'))
+
+        # self._z_plus_button.released.connect(lambda: self.controller.arrow_button_released())
+        # self._z_minus_button.released.connect(lambda: self.controller.arrow_button_released())
+        # self._a_plus_button.released.connect(lambda: self.controller.arrow_button_released())
+        # self._a_minus_button.released.connect(lambda: self.controller.arrow_button_released())
+
+        self._z_plus_button.setIcon(self._z_plus_button.style().standardIcon(QStyle.StandardPixmap.SP_ArrowUp))
+        self._z_minus_button.setIcon(self._z_minus_button.style().standardIcon(QStyle.StandardPixmap.SP_ArrowDown))
+        self._a_plus_button.setIcon(self._a_plus_button.style().standardIcon(QStyle.StandardPixmap.SP_ArrowRight))
+        self._a_minus_button.setIcon(self._a_minus_button.style().standardIcon(QStyle.StandardPixmap.SP_ArrowLeft))
 
         # Layout starts here.
         layout = QVBoxLayout()
 
         tool_bar = QHBoxLayout()
-        tool_bar.addWidget(self.connection_status)
-        tool_bar.addWidget(self.connection_button)
+        tool_bar.addWidget(self._connection_status)
+        tool_bar.addWidget(self._connection_button)
+
+        button_box = QGridLayout()
+        button_box.addWidget(self._z_plus_button, 0, 1)
+        button_box.addWidget(self._z_minus_button, 2, 1)
+        button_box.addWidget(self._magnitude_box, 1, 1)
+        button_box.addWidget(self._a_minus_button, 1, 0)
+        button_box.addWidget(self._a_plus_button, 1, 2)
+
+        bottom_bar = QHBoxLayout()
+        bottom_bar.addWidget(self._jog_cancel_button)
 
         layout.addLayout(tool_bar)
+        layout.addLayout(button_box)
+        layout.addLayout(bottom_bar)
         # Layout ends here.
 
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
-
-        self.thread = None
-        self.worker = None
-
-    def connection_button_callback(self):
-        self.connection_status.setText("Connecting...")
-        self.connection_button.setEnabled(False)
         
-        self.thread, self.work = start_worker(DeviceWorker(), self.connection_result_callback)
-
-    def connection_result_callback(self, result):
-        if result is None:
-            self.connection_status.setText("Connection Failed.")
-            self.connection_button.setEnabled(True)
-        else:
-            self.device_connection = result
-            self.connection_status.setText(f"Connected to {result.name}")
+    def _state_changed(self, states):
+        match states:
+            case CState.DISCONNECTED:
+                self._connection_button.setText("Connect")
+                self._connection_status.setText("Disconnected.")
+                self._connection_button.setEnabled(True)
+            case CState.CONNECTING:
+                self._connection_status.setText("Connecting...")
+                self._connection_button.setEnabled(False)
+            case CState.FAILED:
+                self._connection_button.setText("Connect")
+                self._connection_button.setEnabled(True)
+                self._connection_status.setText("Connection Failed, check physical connection.")
+            case CState.CONNECTED:
+                self._connection_button.setText("Disconnect")
+                self._connection_button.setEnabled(True)
+                self._connection_status.setText("Connected!")
+            case CState.STOPPING:
+                self._connection_button.setEnabled(False)
+                self._connection_status.setText("Disconnecting...")
+            case _:
+                self._connection_button.setText("Unknown State")
+                self._connection_button.setEnabled(False)
+                self._connection_status.setText("Unknown State")
 
     def closeEvent(self, event):
-        if self.device_connection:
-            self.device_connection.close()
-
-        event.accept()
+        self.controller.shutdown(self.close)
+        event.ignore()
         
-
 if __name__ == '__main__':
+    from core.ui_controller import UIController
+
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = MainWindow(UIController())
     window.show()
     sys.exit(app.exec())
